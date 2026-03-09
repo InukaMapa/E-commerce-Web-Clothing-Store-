bff035c7ec6a81957ac8670053872cefc9c9a338
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2026-03-10 00:43:08 +0530

    shop page updated

--- backend/src/routes/recommendationRoutes.js
+++ backend/src/routes/recommendationRoutes.js
@@ -6,9 +6,9 @@ const router = express.Router();
 
 router.get("/:userId", async (req, res) => {
   try {
+
     const userId = req.params.userId;
 
-    // Call ML API
     const response = await axios.get(
       "http://127.0.0.1:8000/recommendations",
       { params: { userId } }
@@ -16,16 +16,18 @@ router.get("/:userId", async (req, res) => {
 
     const productIds = response.data.recommendedProducts;
 
-    // Get product details from MongoDB
     const products = await Product.find({
-      _id: { $in: productIds }
+      productId: { $in: productIds }
     });
 
     res.json(products);
 
   } catch (error) {
+
     console.error(error);
+
     res.status(500).json({ message: "Recommendation error" });
+
   }
 });
 
